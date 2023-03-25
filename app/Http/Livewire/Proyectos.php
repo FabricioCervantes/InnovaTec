1dc8cfb6dd5ab6e8ca0294e8909b37c065f3386b
Fabricio Cervantes <marcofabri6@gmail.com>
2023-03-25 10:45:35 -0600

    .

--- app/Http/Livewire/Proyectos.php
+++ app/Http/Livewire/Proyectos.php
@@ -20,13 +20,13 @@ class Proyectos extends Component
     public function render()
     {
         $proyectos = Proyecto::leftjoin('proyectocalificacionesdetalle', 'proyecto.Id', 'proyectocalificacionesdetalle.IdProyecto')
-            ->leftjoin('escuelas', 'proyecto.IdEscuela', 'escuelas.idEscuela')
+            // ->leftjoin('escuelas', 'proyecto.IdEscuela', 'escuelas.idEscuela')
             ->where('NombreCorto', 'like', '%' . $this->search . '%')
             ->orWhere('NombreDescriptivo', 'like', '%' . $this->search . '%')
             // ->orWhere('Nombre', 'like', '%' . $this->search . '%')
             // ->whereNotNull('NombreCorto')
             // ->orderBy('NombreCorto')
-            ->select('proyecto.NombreCorto', 'proyecto.Id', 'proyecto.NombreDescriptivo', 'proyectocalificacionesdetalle.Calificacion', 'escuelas.Nombre')
+            ->select('proyecto.NombreCorto', 'proyecto.Id', 'proyecto.NombreDescriptivo', 'proyectocalificacionesdetalle.Calificacion')
             ->paginate($this->cant);
 
 

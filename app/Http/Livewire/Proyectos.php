ab2eea689fdc88bf7bca094d320d249be392a559
Fabricio Cervantes <marcofabri6@gmail.com>
2023-03-25 12:27:27 -0600

    .

--- app/Http/Livewire/Proyectos.php
+++ app/Http/Livewire/Proyectos.php
@@ -26,7 +26,7 @@ class Proyectos extends Component
             ->orWhere('NombreDescriptivo', 'like', '%' . $this->search . '%')
             // ->orWhere('Nombre', 'like', '%' . $this->search . '%')
             // ->whereNotNull('NombreCorto')
-            // ->orderBy('NombreCorto')
+            ->orderBy('NombreCorto')
             ->select('proyecto.NombreCorto', 'proyecto.Id', 'proyecto.NombreDescriptivo')
             ->paginate($this->cant);
 

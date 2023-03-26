791c88c0e09973fe876be8c1def58ad2fbb55c2e
Fabricio Cervantes <marcofabri6@gmail.com>
2023-03-26 10:17:00 -0600

    modal movil

--- resources/views/livewire/mostrar.blade.php
+++ resources/views/livewire/mostrar.blade.php
@@ -59,7 +59,7 @@
                     class="inline-flex sm:hidden items-center justify-center px-4 py-2 lg:hover:cursor-pointer bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
                     Cerrar</div>
                 <button wire:click="cerrarModal"
-                    class="hidden sm:inline-flex items-center justify-center px-4 py-2 lg:hover:cursor-pointer bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                    class="hidden sm:flex items-center justify-center px-4 py-2 lg:hover:cursor-pointer bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
                     Cerrar</button>
             </div>
         </x-slot>

127f35a36a23ac0b7ecd3ae26480ad4937365d5b
Fabricio Cervantes <marcofabri6@gmail.com>
2023-03-26 10:14:18 -0600

    modal movil

--- resources/views/livewire/mostrar.blade.php
+++ resources/views/livewire/mostrar.blade.php
@@ -54,9 +54,9 @@
                 <p class="text-sm whitespace-normal text-justify capitalize-first">{{ strip_tags($ResultadosAlcanzar) }}
                 </p>
             </div>
-            <div class="flex md:hover:cursor-pointer justify-end">
+            <div class="flex  justify-end">
                 <div wire:click="cerrarModal"
-                    class="inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                    class="inline-flex items-center justify-center px-4 py-2 lg:hover:cursor-pointer bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
                     Cerrar</div>
             </div>
         </x-slot>

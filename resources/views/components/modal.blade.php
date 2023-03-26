8ad2a09ab5c71a7f5c9d2977fa3943228c279927
Fabricio Cervantes <marcofabri6@gmail.com>
2023-03-26 09:39:04 -0600

    modal movil

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -14,7 +14,7 @@
 
 <div x-data="{ show: @entangle($attributes->wire('model')).defer }" x-on:close.stop="show = false" x-on:keydown.escape.window="show = false" x-show="show"
     id="{{ $id }}"
-    class="jetstream-modal {{ $attributes['class'] }} fixed inset-0 overflow-auto  px-4 py-6 sm:px-0 z-50"
+    class="jetstream-modal {{ $attributes['class'] }} fixed inset-0 overflow-y-auto sm:overflow-x-hidden md:overflow-x-auto  px-4 py-6 sm:px-0 z-50"
     style="display: none;">
     <div x-show="show" class="fixed inset-0 transform transition-all" x-on:click="show = false"
         x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0"
@@ -24,12 +24,10 @@
     </div>
 
     <div x-show="show"
-        class="mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
+        class="mb-6 bg-white rounded-lg p-5 overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
         x-trap.inert.noscroll="show" x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
         x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200"
-        x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-        x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-        {{ $slot }}
+        x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" {{-- x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"> --}} {{ $slot }}
+        </div>
     </div>
-</div>
--- resources/views/livewire/mostrar.blade.php
+++ resources/views/livewire/mostrar.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <x-dialog-modal maxWidth="2xl" wire:model="modal" class="">
+    <x-dialog-modal maxWidth="2xl" wire:model="modal" class="sm:block md:flex items-center">
         <x-slot name="title">Información del proyecto</x-slot>
         <x-slot name="content">
             <div class="flex gap-2 sm:gap-10 sm:justify-between">
@@ -54,9 +54,14 @@
                 <p class="text-sm whitespace-normal text-justify capitalize-first">{{ strip_tags($ResultadosAlcanzar) }}
                 </p>
             </div>
+            <div class="flex justify-end">
+                <div wire:click="cerrarModal"
+                    class="inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150 hover:cursor-pointer">
+                    Cerrar</div>
+            </div>
         </x-slot>
         <x-slot name="footer">
-            <x-danger-button wire:click="cerrarModal">Cerrar</x-danger-button>
+            {{-- <x-danger-button wire:click="cerrarModal">Cerrar</x-danger-button> --}}
         </x-slot>
     </x-dialog-modal>
 

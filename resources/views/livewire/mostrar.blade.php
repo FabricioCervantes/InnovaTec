448b1b7787b21af11b3afbdf68d503d64aac1dbb
Fabricio Cervantes <marcofabri6@gmail.com>
2023-03-26 10:10:36 -0600

    modal movil

--- resources/views/livewire/mostrar.blade.php
+++ resources/views/livewire/mostrar.blade.php
@@ -55,9 +55,9 @@
                 </p>
             </div>
             <div class="flex justify-end">
-                <button wire:click="cerrarModal"
-                    class="inline-flex items-center justify-center px-4 py-2 sm:hover:cursor-pointer bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                    Cerrar</button>
+                <div wire:click="cerrarModal"
+                    class="inline-flex items-center justify-center px-4 py-2 sm:cursor-pointer bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                    Cerrar</div>
             </div>
         </x-slot>
         <x-slot name="footer">

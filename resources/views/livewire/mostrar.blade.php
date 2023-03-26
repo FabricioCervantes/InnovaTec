db46d58c5e45fb82386d6365b93d594febb76ecf
Fabricio Cervantes <marcofabri6@gmail.com>
2023-03-26 10:12:48 -0600

    modal movil

--- resources/views/livewire/mostrar.blade.php
+++ resources/views/livewire/mostrar.blade.php
@@ -56,7 +56,7 @@
             </div>
             <div class="flex justify-end">
                 <div wire:click="cerrarModal"
-                    class="inline-flex items-center justify-center px-4 py-2 hover:cursor-pointer bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                    class="inline-flex items-center justify-center px-4 py-2 md:hover:cursor-pointer bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
                     Cerrar</div>
             </div>
         </x-slot>
